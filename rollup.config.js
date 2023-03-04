92704497a7470a104b2e98691d967ffceac9d28f
Yong-zhi <yongzhi1.liu@gmail.com>
2023-03-04 23:13:34 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import typescript2 from 'rollup-plugin-typescript2';
+import dts from 'rollup-plugin-dts';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'lib/happy-player.cjs.js',
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: 'lib/happy-player.esm.js',
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      typescript2({
+        tsconfig: './tsconfig.json',
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            target: isProduction ? 'es5' : 'es2015'
+          }
+        }
+      })
+    ]
+  },
+  // Build type definitions
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.d.ts',
+      format: 'es'
+    },
+    plugins: [dts()]
+  }
+];
+
+export default config
