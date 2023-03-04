866174be1bbb7045b165d7c9b9ec5c810bca08e7
Yong-zhi <yongzhi1.liu@gmail.com>
2023-03-04 23:58:49 +0800

    types: core types

--- src/types/core.ts
+++ src/types/core.ts
@@ -26,14 +26,50 @@ export interface Data {
 /**
  * 背景音乐
  */
-export interface BackgroundAudio {}
+export interface BackgroundAudio {
+  duration: number
+  source: string
+  startTime: number
+  endTime: number
+  repeat?: boolean
+  mute?: boolean
+  volume: number
+}
 
 /**
  * 场景
  */
-export interface Scene {}
+export interface Scene {
+  duration: number
+  volume: number
+  dub?: Dub
+  subtitle?: Subtitle
+  materials: Material
+}
 
 /**
  * 视频元素
  */
 export interface VideoElement {}
+
+/**
+ * 配音
+ */
+export interface Dub {}
+
+/**
+ * 字幕
+ */
+export interface Subtitle {}
+
+/**
+ * 字幕
+ * @type { number } 1: 视频素材 2: 图片素材
+ */
+export interface Material {
+  type: 1 | 2
+  duration: number
+  startTime?: number
+  endTime?: number
+  volume?: number
+}
