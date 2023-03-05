7601c62d29db79f84e8f2b9138247acc6f6c2594
Yong-zhi <yongzhi1.liu@gmail.com>
2023-03-05 11:57:57 +0800

    feat: add example

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+
 import vue from '@vitejs/plugin-vue'
 import react from '@vitejs/plugin-react-swc'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), react()],
+export default defineConfig(({mode}) => {
+  return {
+    base: '/',
+    root: `./example/${mode}`,
+    plugins: [react(), vue()],
+    server: {
+      port: 9000
+    }
+  }
 })
