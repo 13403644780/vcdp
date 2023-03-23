003c90d9e3025c2872c7b58d3a1fecb52cb6c54e
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-03-23 13:09:54 +0800

    chore: babel terser polyfill

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
 import typescript2 from "rollup-plugin-typescript2"
 import dts from "rollup-plugin-dts"
 import image from "@rollup/plugin-image"
+import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import babel from '@rollup/plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 const isProduction = process.env.NODE_ENV === "production"
 
 const config = [
@@ -8,14 +12,37 @@ const config = [
         input: "src/index.ts",
         output: [
             {
-                file: "lib/happy-player.cjs.js",
+                file: "lib/vcdp.cjs.js",
                 format: "cjs",
                 sourcemap: true,
             },
             {
-                file: "lib/happy-player.esm.js",
+                file: "lib/vcdp.cjs.min.js",
+                format: "cjs",
+                plugins: [terser()]
+            },
+            {
+                file: 'lib/vcdp.umd.js',
+                format: 'umd',
+                name: 'vcdp',
+            },
+            {
+                file: 'lib/vcdp.umd.min.js',
+                format: 'umd',
+                name: 'vcdp',
+                plugins: [
+                    terser()
+                ]
+            },
+            {
+                file: "lib/vcdp.esm.js",
                 format: "esm",
                 sourcemap: true,
+            },
+            {
+                file: "lib/vcdp.esm.min.js",
+                format: "esm",
+                plugins: [terser()],
             },
         ],
         plugins: [
@@ -29,6 +56,14 @@ const config = [
                 },
             }),
             image(),
+            nodeResolve(),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+            }),
+            commonjs({
+                include: 'node_modules/**',
+            })
         ],
     },
     {
