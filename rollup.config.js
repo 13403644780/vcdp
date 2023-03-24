6bf6415d602faef403b3c2649bad9e6d0371a294
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-03-24 22:04:09 +0800

    chore: :hammer: 增加husky

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript2 from "rollup-plugin-typescript2"
 import dts from "rollup-plugin-dts"
 import image from "@rollup/plugin-image"
-import { terser } from 'rollup-plugin-terser'
+import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
