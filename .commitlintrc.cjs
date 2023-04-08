eba45aa248a6234b323c3eb5a491241c4f0e80f6
liuyongzhi <50040262+overdev-l@users.noreply.github.com>
2023-04-08 16:32:28 +0800

    feat: :sparkles: 增加commit lint校验

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,6 +1,7 @@
 // .commitlintrc.js
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
+  extends: ['@commitlint/config-conventional'],
   rules: {
     // @see: https://commitlint.js.org/#/reference-rules
   },
